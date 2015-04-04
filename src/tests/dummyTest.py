04e05f56028a3a64d9a2c5a3509e88ad101e1919
Wojciech Miga <wojciech.miga@gmail.com>
2015-04-04 01:51:12 +0100

    Testy

--- src/tests/dummyTest.py
+++ src/tests/dummyTest.py
@@ -1,1 +1,14 @@
 __author__ = 'wojtek'
+
+import unittest
+
+class FooTests(unittest.TestCase):
+
+    def testFoo(self):
+        self.failUnless(True)
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
