4e38ff5e74e39d75683309e02a54a85836a0b515
Wojciech Miga <wojciech.miga@gmail.com>
2015-08-06 00:12:52 +0100

    Added  setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Feed Reader',
+      version='0.0.1',
+      description='Crossplatform feed reader',
+      author='Wojciech Miga',
+      author_email='wojciech.miga@gmail.command',
+      packages=['distutils', 'distutils.command'],
+     )
